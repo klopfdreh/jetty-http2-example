55fe598faecd5c32df94e199f4f472f395466c32
Tobias Soloschenko <tsoloschenko@apache.org>
2016-05-13 09:29:02 +0200

    Check for http/2 protocol

--- de.jetty.wicket.http2.example/src/main/java/de/jetty/wicket/http2/example/PushHeaderItem.java
+++ de.jetty.wicket.http2.example/src/main/java/de/jetty/wicket/http2/example/PushHeaderItem.java
@@ -19,12 +19,20 @@ import org.eclipse.jetty.server.Request;
  * A push header item to be used in the http/2 context and to reduce the latency of the web
  * application
  * 
+ * TODO org.eclipse.jetty.server.Request has to be replaced with the final javax.servlet.api
+ * interface (HttpServletRequest)
+ * 
  * @author Tobias Soloschenko
  *
  */
 public class PushHeaderItem extends HeaderItem
 {
 	private static final long serialVersionUID = 1L;
+
+	/**
+	 * The http2 protocol string
+	 */
+	public static final String HTTP2_PROTOCOL = "http/2";
 
 	private Set<String> urls = new ConcurrentHashSet<String>(new TreeSet<String>());
 
@@ -43,11 +51,14 @@ public class PushHeaderItem extends HeaderItem
 	@Override
 	public void render(Response response)
 	{
-		for (String url : urls)
+		Request request = getContainerRequest(RequestCycle.get().getRequest());
+		// Check if the protocol is http/2 or http/2.0 to only push the resources in this case
+		if (isHttp2(request))
 		{
-			// TODO This has to be replaced with the final javax.servlet.api interface (HttpServletRequest)
-			((Request)RequestCycle.get().getRequest().getContainerRequest()).getPushBuilder()
-				.path(url.toString()).push();
+			for (String url : urls)
+			{
+				request.getPushBuilder().path(url.toString()).push();
+			}
 		}
 	}
 
@@ -55,56 +66,81 @@ public class PushHeaderItem extends HeaderItem
 	 * Creates a URL and pushes the resource to the client - this is only supported if http2 is
 	 * enabled
 	 * 
-	 * @param object
-	 *            the object to create the URL for - if it is a String this is taken as a URL
-	 *            directly
-	 * @param parameters
-	 *            the parameters to be applied to create the URL
+	 * @param pushItems
+	 *            a list of items to be pushed to the client
+	 * @return the current push header item
 	 */
 	@SuppressWarnings("unchecked")
 	public PushHeaderItem push(List<PushItem> pushItems)
 	{
-		for (PushItem pushItem : pushItems)
-		{
-			Object object = pushItem.getObject();
-			PageParameters parameters = pushItem.getPageParameters();
+		RequestCycle requestCycle = RequestCycle.get();
+		if (isHttp2(getContainerRequest(requestCycle.getRequest())))
+			for (PushItem pushItem : pushItems)
+			{
+				Object object = pushItem.getObject();
+				PageParameters parameters = pushItem.getPageParameters();
 
-			if (object == null)
-			{
-				throw new WicketRuntimeException(
-					"Please provide an object to the items to be pushed, so that the url can be created for the given resource.");
-			}
+				if (object == null)
+				{
+					throw new WicketRuntimeException(
+						"Please provide an object to the items to be pushed, so that the url can be created for the given resource.");
+				}
 
-			RequestCycle requestCycle = RequestCycle.get();
-			CharSequence url = null;
-			if (object instanceof ResourceReference)
-			{
-				url = requestCycle.urlFor((ResourceReference)object, parameters);
-			}
-			else if (object instanceof Class)
-			{
-				url = requestCycle.urlFor((Class<? extends Page>)object, parameters);
-			}
-			else if (object instanceof IRequestHandler)
-			{
-				url = requestCycle.urlFor((IRequestHandler)object);
-			}
-			else
-			{
-				url = object.toString();
-			}
+				CharSequence url = null;
+				if (object instanceof ResourceReference)
+				{
+					url = requestCycle.urlFor((ResourceReference)object, parameters);
+				}
+				else if (object instanceof Class)
+				{
+					url = requestCycle.urlFor((Class<? extends Page>)object, parameters);
+				}
+				else if (object instanceof IRequestHandler)
+				{
+					url = requestCycle.urlFor((IRequestHandler)object);
+				}
+				else
+				{
+					url = object.toString();
+				}
 
-			if (url.toString().equals("."))
-			{
-				url = "/";
-			}
-			else if (url.toString().startsWith("."))
-			{
-				url = url.toString().substring(1);
-			}
+				if (url.toString().equals("."))
+				{
+					url = "/";
+				}
+				else if (url.toString().startsWith("."))
+				{
+					url = url.toString().substring(1);
+				}
 
-			urls.add(url.toString());
-		}
+				urls.add(url.toString());
+			}
 		return this;
+	}
+
+	/**
+	 * Gets the container request
+	 * 
+	 * @param request
+	 *            the wicket request to get the container request from
+	 * @return the container request
+	 */
+	public Request getContainerRequest(org.apache.wicket.request.Request request)
+	{
+
+		return (Request)request.getContainerRequest();
+	}
+
+	/**
+	 * Checks if the given request is a http/2 request
+	 * 
+	 * @param request
+	 *            the request to check if it is a http/2 request
+	 * @return if the request is a http/2 request
+	 */
+	public boolean isHttp2(Request request)
+	{
+		// detects http/2 and http/2.0
+		return request.getProtocol().toLowerCase().contains(HTTP2_PROTOCOL);
 	}
 }
